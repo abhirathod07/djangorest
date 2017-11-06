cf615ddd52bd7f78ae500b52a2ced090adca7d5a
abhirathod7 <abhishekrathod7@gmail.com>
2017-11-06 23:02:42 +0530

    updated settings file

--- restapisecond/restapisecond/settings.py
+++ restapisecond/restapisecond/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '@7^%h2l_mg&#ggazsu5_#+ay09mrtg0zq#)d#29o55*_gnm0$-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['morning-basin-25829.herokuapp.com']
+ALLOWED_HOSTS = ['cryptic-earth-77872.herokuapp.com']
 
 
 # Application definition
